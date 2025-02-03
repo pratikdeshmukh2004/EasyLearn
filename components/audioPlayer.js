f5e09366e586ad1ba7a59f102fd53c56ad498390
Pratik Deshmukh <pratikdeshmukhlobhi@gmail.com>
2025-02-03 11:41:39 +0530

    audio with internal

--- components/audioPlayer.js
+++ components/audioPlayer.js
@@ -1,77 +1,139 @@
-import { faVolumeHigh, faVolumeLow } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGauge,
+  faMinusSquare,
+  faPlusSquare,
+  faVolumeHigh,
+  faVolumeLow,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Lottie from "react-lottie";
 import audioData from "../animations/audio_playing.json";
 
 const AudioController = ({ link }) => {
-  const audioRef = useRef(null); // Reference to the audio element
+  const [audio, setAudio] = useState(new Audio(link)); // Store Audio object in state
   const [isPlaying, setIsPlaying] = useState(false);
   const [volume, setVolume] = useState(0.75); // Default volume (1 = 100%)
-
-  // Handle volume change
-  const handleVolumeChange = (event) => {
-    const newVolume = event.target.value;
-    audioRef.current.volume = newVolume;
-    setVolume(newVolume);
-  };
+  const [speed, setSpeed] = useState(1);
 
   useEffect(() => {
-    audioRef.current.pause();
+    audio.pause();
+    setAudio(new Audio(link));
     setIsPlaying(false);
   }, [link]);
 
+  useEffect(() => {
+    audio.volume = volume;
+  }, [volume]);
+
+  useEffect(() => {
+    audio.playbackRate = speed;
+  }, [speed]);
+
+  // Handle play/pause functionality
+  const handlePlay = () => {
+    audio.play();
+    setIsPlaying(true);
+  };
+
+  const handlePause = () => {
+    audio.pause();
+    setIsPlaying(false);
+  };
+
+  const changeVolume = (value) => {
+    const newVolume = Math.min(1, Math.max(0, volume + value));
+    setVolume(newVolume);
+  };
+
+  const changeSpeed = (value) => {
+    const newSpeed = Math.min(2, Math.max(0.5, speed + value));
+    setSpeed(newSpeed);
+  };
+
   return (
     <div className="shadow-custom rounded-lg mt-5 p-5">
-      <audio ref={audioRef} className="hidden" src={link}></audio>
       <div className="flex items-center justify-between">
         <button
-          onClick={() => {
-            audioRef.current.play();
-            setIsPlaying(true);
-          }}
+          onClick={handlePlay}
           className="bg-black hover:bg-gray-700 p-3 px-6 rounded-lg text-white text-sm font-medium"
         >
           Play
         </button>
-        <select>
-          <option>Speed</option>
-          <option>1x</option>
-        </select>
-
         <button
-          onClick={() => {
-            audioRef.current.pause();
-            setIsPlaying(false);
-          }}
+          onClick={handlePause}
           className="bg-black hover:bg-gray-700 p-3 px-6 rounded-lg text-white text-sm font-medium"
         >
           Pause
         </button>
       </div>
-      <div className="mt-6 flex gap-2 justify-between items-center">
-        <label htmlFor="volume">
-          <FontAwesomeIcon icon={faVolumeLow} />
-        </label>
-        <input
-          type="range"
-          id="volume"
-          min="0"
-          max="1"
-          step="0.1"
-          value={volume}
-          className="w-9/12 h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer accent-gray-800"
-          style={{
-            background: `linear-gradient(to right, #424242 0%, #424242 ${
-              volume * 100
-            }%, #ddd ${volume * 100}%, #ddd 100%)`,
-          }}
-          onChange={handleVolumeChange}
+      <p htmlFor="speed" className="text-sm mt-3 mx-auto text-center">
+        Volume {Math.round(volume * 100)}%
+      </p>
+      <div className="flex gap-2 justify-between items-center relative">
+        <FontAwesomeIcon
+          size="2xl"
+          className="cursor-pointer"
+          onClick={() => changeVolume(-0.1)}
+          icon={faMinusSquare}
         />
-        {volume * 100}%
-        <label htmlFor="volume">
-          <FontAwesomeIcon icon={faVolumeHigh} />
-        </label>
+        <div className="relative w-8/12">
+          <input
+            type="range"
+            id="volume"
+            min="0"
+            max="1"
+            step="0.1"
+            value={volume}
+            className="w-full h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer accent-gray-800"
+            style={{
+              background: `linear-gradient(to right, #424242 0%, #424242 ${
+                volume * 100
+              }%, #ddd ${volume * 100}%, #ddd 100%)`,
+            }}
+            onChange={(e) => setVolume(parseFloat(e.target.value))}
+          />
+        </div>
+        <FontAwesomeIcon
+          onClick={() => changeVolume(0.2)}
+          className="cursor-pointer"
+          size="2xl"
+          icon={faPlusSquare}
+        />
+      </div>
+      <p htmlFor="speed" className="text-sm mt-3 mx-auto text-center">
+        Speed {speed}x
+      </p>
+      <div className="flex gap-2 justify-between items-center relative">
+        <FontAwesomeIcon
+          size="2xl"
+          onClick={() => changeSpeed(-0.5)}
+          icon={faMinusSquare}
+          className="cursor-pointer"
+        />
+        <div className="relative w-8/12">
+          <input
+            type="range"
+            id="speed"
+            min="0.5"
+            max="2"
+            step="0.5"
+            value={speed}
+            style={{
+              background: `linear-gradient(to right, #424242 0%, #424242 ${
+                ((speed - 0.5) / 1.5) * 100
+              }%, #ddd ${((speed - 0.5) / 1.5) * 100}%, #ddd 100%)`,
+            }}
+            className="w-full h-2 bg-gray-300 rounded-lg appearance-none cursor-pointer accent-gray-800"
+            onChange={(e) => setSpeed(parseFloat(e.target.value))}
+          />
+        </div>
+        <FontAwesomeIcon
+          onClick={() => changeSpeed(+0.5)}
+          className="cursor-pointer"
+          size="2xl"
+          icon={faPlusSquare}
+        />
       </div>
     </div>
   );
